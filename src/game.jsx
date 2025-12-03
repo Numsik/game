e2131b7c8e88d3f0092a31fc150fabc422f71ea0
Numsik <honzik.klusek.myto@gmail.com>
2025-12-03 14:55:20 +0100

    version 2

--- src/game.jsx
+++ src/game.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react"
+import { useState,useEffect, useRef } from "react"
 
 export function Game(){
     const [position, setposition] = useState({x: 0, y: 0});
+    const [score, setScore] = useState(0)
+    const intervalik = useRef(null);
+
+    const addscore = () =>{
+        setScore(s => s + 1)
+    }
 
     const handleMove = (e) => {
         setposition({
@@ -9,21 +15,43 @@ export function Game(){
             y: e.clientY
         })
     }
+
+    //random funkce na x a y
     const randomcisla = () =>{
-        setposition({x: Math.floor(Math.random() * 450) + 390, 
-                    y: Math.floor(Math.random() * 450) + 95})
+        setposition({x: Math.floor(Math.random() * 901) + 400, 
+                    y: Math.floor(Math.random() * 450) + 100})
 
     }
+    const stopBall = () =>{
+        if (intervalik.current){
+            clearInterval(intervalik.current);
+            intervalik.current = null;
+        }
+    };
+
+    //nacitani koule
+    useEffect(() =>{
+        const interval = setInterval(() =>{
+        randomcisla()
+        console.log("tf")
+        },1000);
+
+        intervalik.current = interval
+        return () => clearInterval(interval)
+    }, [])
+    
 
     return(<>
     <div className="board">
-        <div className="points" style={{
+        <div  onClick={addscore} className="points" style={{
             position: "absolute",
             left: `${position.x}px`,
             top: `${position.y}px`
         }}></div>
     </div>
     <p>Pozice x: {position.x} pozice y: {position.y}</p>
+    <p>score: {score}</p>
     <button onClick={randomcisla}>change position</button>
+    <button onClick={stopBall}>Stop ball</button>
     </>)
 }
